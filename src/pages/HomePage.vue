9f0070787ae911e0788294ab673601aba20ae683
IsaiahWright902 <icwright902@gmail.com>
2022-01-21 11:47:37 -0500

    Removed Image

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,22 +7,9 @@
     </div>
     <div class="row pt-5">
       <div class="col-12 text-center">
-        <p>
-          This is a very lightweight vite build. Built for speed and compression
-          including:
-        </p>
-        <p>Bootstrap</p>
-        <p>Sass</p>
-        <p>Sweet Alert</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12 text-center">
-        <img
-          class="img-fluid"
-          src="https://ih1.redbubble.net/image.652172967.8484/flat,750x,075,f-pad,750x1000,f8f8f8.u6.jpg"
-          alt=""
-        />
+        <p>This is a very lightweight vite build built for speed including:</p>
+        <p>Bootstrap | Sass | Sweet Alert</p>
+
       </div>
     </div>
   </div>
