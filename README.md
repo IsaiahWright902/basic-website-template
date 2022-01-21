451617aa4d14f1a50223e8340a1c41bbf70a71b2
IsaiahWright902 <icwright902@gmail.com>
2022-01-21 09:30:15 -0500

    addding dist

--- README.md
+++
@@ -1,5 +0,0 @@
-# Basic Website Template
-
-This is a template for static websites that includes
-
-Vite | Vue 3 | Bootstrap 5 | Sass | Sweet Alert 2
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { createApp } from "vue";
-import App from "./App.vue";
-import { router } from "./router";
-
-const root = createApp(App);
-
-root.use(router).mount("#app");
