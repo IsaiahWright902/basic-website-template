efda487eed1cc3aa37b25e2b358678bfa5dba868
IsaiahWright902 <icwright902@gmail.com>
2022-02-02 14:11:24 -0500

    port configured

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/basic-website-template/",
+  // base: "/basic-website-template/",
+  server: {
+    port: "8080",
+  },
 });
